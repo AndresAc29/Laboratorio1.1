0d2813db1aa4f27ac1fea6a046f03c3b715a2956
Andre <Andre@Dress-XC29>
2021-05-22 22:31:23 -0600

    este git es para realizar el laboratorio numero 1

---
+++ src/main/java/NewClass.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Andre
+ */
+public class NewClass {
+
+    public void Saludo(){
+        
+        System.out.println("HOLA MUNDO");
+        
+    }
+    
+    public static void main(String[] args) {
+        NewClass pr1= new NewClass();
+        pr1.Saludo();
+
+    }
+    
+}
